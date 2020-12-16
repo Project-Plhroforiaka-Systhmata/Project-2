1c892d5ba04a390e05a73d4b4a24c66e57c10bae
treloubas76 <chris96skan@gmail.com>
2020-12-16 13:32:56 +0200

    add tests to make file

---
+++ cmake-build-debug/CMakeFiles/Project-2.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/treloubas76/CLionProjects/Project-2/bucket.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/bucket.cpp.o"
+  "/home/treloubas76/CLionProjects/Project-2/hashTable.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/hashTable.cpp.o"
+  "/home/treloubas76/CLionProjects/Project-2/list.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/list.cpp.o"
+  "/home/treloubas76/CLionProjects/Project-2/main.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/main.cpp.o"
+  "/home/treloubas76/CLionProjects/Project-2/negativeList.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/negativeList.cpp.o"
+  "/home/treloubas76/CLionProjects/Project-2/vector.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/vector.cpp.o"
+  "/home/treloubas76/CLionProjects/Project-2/vertex.cpp" "/home/treloubas76/CLionProjects/Project-2/cmake-build-debug/CMakeFiles/Project-2.dir/vertex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project-2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project-2.dir/bucket.cpp.o"
+  "CMakeFiles/Project-2.dir/hashTable.cpp.o"
+  "CMakeFiles/Project-2.dir/list.cpp.o"
+  "CMakeFiles/Project-2.dir/main.cpp.o"
+  "CMakeFiles/Project-2.dir/negativeList.cpp.o"
+  "CMakeFiles/Project-2.dir/vector.cpp.o"
+  "CMakeFiles/Project-2.dir/vertex.cpp.o"
+  "Project-2"
+  "Project-2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project-2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
